0732f907480d53aed7516f6d09469fe8c05ddc04
Karim Aboelazm <karimaboelazm28@gmail.com>
2021-12-17 04:40:07 +0200

    adding some setteng

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('dj_rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
     
     path('', include('ecommerce.urls',namespace='ecom')),
     
